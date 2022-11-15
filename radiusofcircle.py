4f1f9d6e7248598cb54fa81ea6109ccc2d844b86
AdusumilliNithin <118040849+AdusumilliNithin@users.noreply.github.com>
2022-11-15 16:44:32 +0530

    Add files via upload

---
+++ radiusofcircle.py
@@ -0,0 +1,6 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=============== RESTART: C:/Users/nithi/Desktop/radiusofcircle.py ==============
+Input the radius of the circle : 4
+The area of the circle with radius 4.0 is: 50.26548245743669
